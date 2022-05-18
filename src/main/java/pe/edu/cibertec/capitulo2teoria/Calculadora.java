73730c14072a9dad621f78bcc969b4b81a5ab514
yuri <yuri@192.168.1.99>
2022-05-17 23:24:16 -0500

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/java/pe/edu/cibertec/capitulo2teoria/Calculadora.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.cibertec.capitulo2teoria;
+
+/**
+ *
+ * @author yuri
+ */
+public class Calculadora {
+
+    public int sumar(int sumando1, int sumando2) {
+        int resultado;
+        resultado = sumando1 + sumando2;
+        return resultado;
+
+    }
+
+    public int restar(int minuedo, int sustraendo) {
+        int resultado;
+        resultado = minuedo - sustraendo;
+        return resultado;
+    }
+
+    public int multiplicar(int multiplicador1, int multiplicador2) {
+        int resultado;
+        resultado = multiplicador1 * multiplicador2;
+        return resultado;
+    }
+
+  /*  public int dividir(int operador1, int operador2) {
+        int resultado;
+        resultado = operador1 / operador2;
+        return resultado;
+    }
+  */
+
+
+}
